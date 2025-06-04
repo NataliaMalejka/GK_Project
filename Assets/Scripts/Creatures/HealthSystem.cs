6c7faf4b3b36c0fca8a193cb736429a7352d3685
NataliaMalejka <natalia280803@gmail.com>
2025-06-04 19:04:47 +0200

    FireBullet

--- Assets/Scripts/Creatures/HealthSystem.cs
+++ Assets/Scripts/Creatures/HealthSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -21,6 +22,23 @@ public class HealthSystem : MonoBehaviour
             currentHelath = 0;
             Die();
         }               
+    }
+
+    public void ApplyDamageOverTime(int damagePerSecond, int duration)
+    {
+        StartCoroutine(DamageOverTimeCoroutine(damagePerSecond, duration));
+    }
+
+    private IEnumerator DamageOverTimeCoroutine(int damagePerSecond, int duration)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            GetDmg(damagePerSecond);
+            yield return new WaitForSeconds(1f);
+            elapsed += 1f;
+        }
     }
 
     public void Heal(int amound)
