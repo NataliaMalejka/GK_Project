eab3286827c7826e4bd51cbcf65768f5c9869d41
David-11235813 <dddulian10@gmail.com>
2025-06-11 23:10:07 +0200

    Connected HUD to Mana & Stamina systems

--- Assets/Scripts/Player Scripts/ManaSystem.cs
+++ Assets/Scripts/Player Scripts/ManaSystem.cs
@@ -1,18 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManaSystem : MonoBehaviour
 {
     [SerializeField] private int maxMana;
-    public int currentmMna;
+    public int currentMana;
+
+    [Header("UI (auto-found at runtime)")]
+    [SerializeField] private Image manaFillBar;
 
     private void Awake()
     {
-        currentmMna = maxMana;
+        // find the canvas root once
+        var hudGO = GameObject.FindGameObjectWithTag("HUD_Canvas");
+        if (hudGO == null)
+        {
+            Debug.LogError("HUD_Canvas tag missing on your HUD!");
+            return;
+        }
+
+        // now find your specific child by name:
+        var staminaTransform = hudGO.transform.Find("Mana/Mana-fill");
+        if (staminaTransform != null)
+            manaFillBar = staminaTransform.GetComponent<Image>();
+        else
+            Debug.LogError("Could not find Mana/Mana-fill under HUD!");
+
+
+        currentMana = maxMana;
+        updateManaBar();
     }
 
     public bool CanReduceMana(int amound)
     {
-        if (currentmMna >= amound)
+        if (currentMana >= amound)
             return true;
         else
             return false;
@@ -20,20 +41,31 @@ public class ManaSystem : MonoBehaviour
 
     public void ReudceMana(int amound)
     {
-        currentmMna -= amound;
+        currentMana -= amound;
+        updateManaBar();
     }    
 
     public void IncreaseCurrentMana(int amound)
     {
-        currentmMna += amound;
+        currentMana += amound;
 
-        if(currentmMna > maxMana)
-            currentmMna = maxMana;
+        if(currentMana > maxMana)
+            currentMana = maxMana;
+
+        updateManaBar();
     }
 
     public void IncreaseMaxMana(int amound)
     {
         maxMana += amound;
         IncreaseCurrentMana(amound);
+
+        updateManaBar();
+    }
+
+
+    private void updateManaBar()
+    {
+        manaFillBar.fillAmount = currentMana / maxMana;
     }
 }
--- Assets/Scripts/Player Scripts/StaminaSystem.cs
+++ Assets/Scripts/Player Scripts/StaminaSystem.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StaminaSystem : MonoBehaviour, IFixedUpdateObserver
 {
     [SerializeField] private float staminaIncrease;
     [SerializeField] private float maxStamina;
     public float currentStamina;
+
+    [Header("UI (auto-found at runtime)")]
+    [SerializeField] private Image staminaFillBar;
+
 
     private void OnEnable()
     {
@@ -18,7 +23,24 @@ public class StaminaSystem : MonoBehaviour, IFixedUpdateObserver
 
     private void Awake()
     {
+        // find the canvas root once
+        var hudGO = GameObject.FindGameObjectWithTag("HUD_Canvas");
+        if (hudGO == null)
+        {
+            Debug.LogError("HUD_Canvas tag missing on your HUD!");
+            return;
+        }
+
+        // now find your specific child by name:
+        var staminaTransform = hudGO.transform.Find("Stamina/Stamina-fill");
+        if (staminaTransform != null)
+            staminaFillBar = staminaTransform.GetComponent<Image>();
+        else
+            Debug.LogError("Could not find Stamina/Stamina-fill under HUD!");
+
+
         currentStamina = maxStamina;
+        updateStaminaBar();
     }
 
     public void ObserveFixedUpdate()
@@ -29,6 +51,7 @@ public class StaminaSystem : MonoBehaviour, IFixedUpdateObserver
         {
             currentStamina = maxStamina;
         }
+        updateStaminaBar();
     }
 
     public bool CanReduceStamina(float amound)
@@ -42,16 +65,25 @@ public class StaminaSystem : MonoBehaviour, IFixedUpdateObserver
     public void ReduceStamina(float amound)
     {
         currentStamina -= amound;
+        updateStaminaBar();
     }
 
     public void IncreaseStamina(float amound)
     {
         currentStamina += amound;
+        updateStaminaBar();
     }
 
     public void IncreaseMaxStamnina(float amound)
     {
         maxStamina += amound;
         currentStamina += amound;
+        updateStaminaBar();
+    }
+
+
+    private void updateStaminaBar()
+    {
+        staminaFillBar.fillAmount = currentStamina / maxStamina;
     }
 }
